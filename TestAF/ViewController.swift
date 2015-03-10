69624c4eaa5040eb592cd252e9ab1c031802f5db
James Tang <j@jamztang.com>
2015-03-10 12:12:14 +0800

    Initial Commit

---
+++ TestAF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAF
+//
+//  Created by James Tang on 10/3/15.
+//  Copyright (c) 2015 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
